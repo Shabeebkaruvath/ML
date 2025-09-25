8a0e216249e2950271296edc3b2c3c5a1b3175d8
shabeebkaruvath <shabeebkaruvath@gmail.com>
2025-09-25 11:31:38 +0530

    lab2_1

--- python/labcycle2/1.py
+++ python/labcycle2/1.py
@@ -7,11 +7,11 @@ for i in range(lim):
     word=input("Enter a word:")
     words.append(word)
 
-word=input("enter the the word to search:")
+term=input("enter the the word to search:")
 
-for i, data in enumerate(words,start=1):
-    if word in data:
-        index.append(i+1)
+for i, data in enumerate(words):
+    if term in data:
+        index.append(i)
         words[i]="1"
     else:
        words[i]="0"
@@ -22,30 +22,3 @@ print("containing word index value changed:")
 print(words)
  
  
-lim = int(input("Enter the limit: "))
-
-words = []
-index = []
-
-# Collect words
-for i in range(lim):
-    word = input("Enter a word: ")
-    words.append(word)
-
-# Word to search for
-word_to_find = input("Enter the word to search for: ")
-
-# Iterate and find indices of words containing the substring
-for i, data in enumerate(words):
-    if word_to_find in data:
-        index.append(i + 1)  # Append 1-based index
-        words[i] = "1"       # Change the word to "1"
-    else:
-        words[i] = "0"       # Change the word to "0"
-
-# Print results
-print("Containing word index positions are:")
-print(index)
-
-print("Containing word index values changed:")
-print(words)
